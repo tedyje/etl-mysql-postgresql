abd78f3b56b2667684a7534e5b772a90b4117057
Tewodros Jemberu <tedyje@gmail.com>
2021-01-17 17:53:48 -0500

    Initial Commit

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+DB_DETAILS = {
+
+    'dev' : {
+        'SOURCE_DB': {
+            'DB_TYPE' : 'mysql', 
+            'DB_HOST' : '127.0.0.1',
+            'DB_NAME' : 'retail_db',
+            'DB_USER' : os.environ.get('SOURCE_DB_USER'),
+            'DB_PASS' : os.environ.get('SOURCE_DB_PASS')
+        },
+
+        'TARGET_DB' : {
+            'DB_TYPE' : 'mysql', 
+            'DB_HOST' : '127.0.0.1',
+            'DB_NAME' : 'retail_db',
+            'DB_USER' : os.environ.get('TARGET_DB_USER'),
+            'DB_PASS' : os.environ.get('TARGET_DB_PASS')
+        }
+    }
+}
---
+++ util.py
@@ -0,0 +1,7 @@
+from mysql import connector as mc
+from mysql.connector import errorcode as ec
+import psycopg2 
+import pandas as pd
+from config import DB_DETAILS
+
+
